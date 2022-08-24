ae1417c166a98a66122a8b383126e1ad56932333
CarlosCoelhoSL <ccoelho@scottlogic.com>
2022-08-24 10:13:15 +0100

    Split game, board and square into different files

--- src/index.js
+++ src/index.js
@@ -1,48 +1,8 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Board from './board'; 
 
-
-function Square (props) {
-    return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-      </button>
-    );
-  }
-  
-function Board (props) {
-  const renderSquare = (i) => {
-    return (
-      <Square
-        value={props.squares[i]}
-        onClick={() => props.onClick(i)}
-      />
-    );
-  }
-  
-    return (
-      <div>
-        <div className="board-row">
-          {renderSquare(0)}
-          {renderSquare(1)}
-          {renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {renderSquare(3)}
-          {renderSquare(4)}
-          {renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {renderSquare(6)}
-          {renderSquare(7)}
-          {renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-  
-  
 function Game () {
   const [state, setState] = useState({
     history: [
